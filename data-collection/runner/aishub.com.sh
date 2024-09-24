bd676e89f1cd50179b1b0ac6bea3f460682abefb
Cong Le <congmb@gmail.com>
2024-09-24 21:16:48 +0200

    edit data-cleaning

--- data-collection/runner/aishub.com.sh
+++ data-collection/runner/aishub.com.sh
@@ -6,7 +6,9 @@
 # Supply your own AIS Hub API username
 USERNAME=AH_TRIAL_7EBC4392
 TIME=`date '+%F_%H.%M.%S'`
-FILE_NAME="./data/aishub.com/aishub-${TIME}.csv"
+FILE_NAME="/home/cong/p52t/Code/ovd-framework/data-collection/raw_files/aishub-${TIME}.csv"
 
 # Retrieve data
 wget -q -O $FILE_NAME "http://data.aishub.net/ws.php?username=${USERNAME}&format=1&output=csv&compress=0" 
+
+echo $FILE_NAME
